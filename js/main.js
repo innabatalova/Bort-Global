de9ef8d33bba72a35b9cffb1f09c6dca16620029
innabatalova <batalovainna97@gmail.com>
2021-07-26 21:17:46 +0300

    Added: create header desctop

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  //console.log("Hello");
+
+let chooseLanguage = $(".arrow")
+
+chooseLanguage.on("click", function(){
+  //  console.log ("Клик по кнопке меню")
+   $(".language__select").slideToggle(550)
+  
+
+});
+
+
+
+});
