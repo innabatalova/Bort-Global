ea13fd4965ff275ef111c48c11b44684d9aa7612
innabatalova <batalovainna97@gmail.com>
2021-07-30 17:59:23 +0300

    Create: added banner

--- js/main.js
+++ js/main.js
@@ -13,13 +13,24 @@ chooseLanguage.on("click", function(){
 });
 
 
-//создание кликабельности для всего блока карточки
+//создание кликабельности для всего блока карточки categories
 
 let linkCard = $(".categories-card")
 
 linkCard.on("click", function(){
   //  console.log ("Клик по кнопке меню")
   location.href='#' //ссылки на странички категорий
+
+});
+
+
+//создание кликабельности стрелочки banner с открытием в новой вкладке
+
+let linkArrow = $(".banner__arrow")
+
+linkArrow.on("click", function(){
+  //  console.log ("Клик по кнопке меню")
+  window.open ('#') //ссылки на странички категорий
 
 });
 
