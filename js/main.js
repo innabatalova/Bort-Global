e03ec5bff14a43dbcfce1cbb5bc9acedc1c9b26b
innabatalova <batalovainna97@gmail.com>
2021-10-04 16:30:45 +0300

    Fix: check click on news-view-button

--- js/main.js
+++ js/main.js
@@ -676,16 +676,10 @@ $('.related-slider').owlCarousel({
 
 //ссылка по клику на кнопку "news-view-button"
 
-  // let newsButtonLink = $(".news-view-button")
-
-  // newsButtonLink.on("click", function () {
-    
-  //   location.href = 'news-preview.html'
-
-  // });
-
   $(".news-view-button").click(function(){
     location.href = 'news-preview.html'
 });
+
+
 
 });
