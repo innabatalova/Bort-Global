669013d136908e02b6de4df2efee833f514001c9
innabatalova <batalovainna97@gmail.com>
2021-10-25 22:13:29 +0300

    Create: added hand parallax for rich

--- js/main.js
+++ js/main.js
@@ -889,17 +889,65 @@ $('.cabinet-menu-item').on('mouseup', function(e){
 // });
 
 //не применяется на рабочих страничках (удалить при необходимости)
- let scrollParallax = $(".rich-section-three");
+ let scrollParallax = $(".parallax-section");
  let counter = 0;
+ let x = 130;
  
  $(window).scroll(function() {
    let scroll = $(window).scrollTop() + $(window).height();
-   let offset = scrollParallax.offset().top;
+   let offset = scrollParallax.offset().top + x;
 
   if(scroll > offset && counter == 0){
-     $(".rich-section__img").css({'transform':'translateY(-65px)'});
+     $(".parallax-object").css({'transform':'translateY(-55px)'});
      counter = 0;
-    } 
+    } else {
+      $(".parallax-object").css({'transform':'translateY(0)'});
+    }
+  
+});
+
+let scrollParallax2 = $(".parallax-section2");
+ 
+ $(window).scroll(function() {
+   let scroll2 = $(window).scrollTop() + $(window).height();
+   let offset2 = scrollParallax2.offset().top + x;
+
+  if(scroll2 > offset2 && counter == 0){
+     $(".parallax-object2").css({'transform':'translateY(-55px)'});
+     counter = 0;
+    } else {
+      $(".parallax-object2").css({'transform':'translateY(0)'});
+    }
+  
+});
+
+let scrollParallax3 = $(".parallax-section3");
+ 
+ $(window).scroll(function() {
+   let scroll3 = $(window).scrollTop() + $(window).height();
+   let offset3 = scrollParallax3.offset().top + x;
+
+  if(scroll3 > offset3 && counter == 0){
+     $(".parallax-object3").css({'transform':'translateY(-55px)'});
+     counter = 0;
+    } else {
+      $(".parallax-object3").css({'transform':'translateY(0)'});
+    }
+  
+});
+
+let scrollParallax4 = $(".parallax-section4");
+ 
+ $(window).scroll(function() {
+   let scroll4 = $(window).scrollTop() + $(window).height();
+   let offset4 = scrollParallax4.offset().top;
+
+  if(scroll4 > offset4 && counter == 0){
+     $(".parallax-object4").css({'transform':'translateY(-55px)'});
+     counter = 0;
+    } else {
+      $(".parallax-object4").css({'transform':'translateY(0)'});
+    }
   
 });
 
